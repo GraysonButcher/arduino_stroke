0c52cc67fac37a67bbcf04ec0928fa39d79b71c7
Damon Becker <dbecker@webroot.com>
2021-06-14 15:22:14 -0600

    Asynchronous code that will run with Python 3.7

--- python/async.py
+++ python/async.py
@@ -1,27 +1,50 @@
-import concurrent.futures
+# import concurrent.futures
+# import time
+
+# done = False
+
+# def blah():
+#   time.sleep(5)
+#   return "done with blah"
+
+# def foo(bar):
+#   time.sleep(10)
+#   return "Returned value: {}".format(bar)
+
+# def cb():
+#   done = True
+
+# executor = concurrent.futures.ThreadPoolExecutor()
+# # thread = executor.submit(foo, args=('done!'))
+# thread = executor.submit(blah)
+# print(thread.result())
+# # thread.add_done_callback(cb)
+
+# while thread.running():
+#   print(done)
+#   time.sleep(1)
+
+# print("Thread is no longer running, global is now {}".format(done))
+
 import time
+import asyncio
 
 done = False
 
-def blah():
-  time.sleep(5)
+async def foo(bar):
+  done = True
+  return "done with foo"
+
+async def blah():
+  done = True
   return "done with blah"
 
-def foo(bar):
-  time.sleep(10)
-  return "Returned value: {}".format(bar)
+async def a_main():
+  task = asyncio.ensure_future(blah())
+  print("In main")
+  await task
 
-def cb():
-  done = True
-
-executor = concurrent.futures.ThreadPoolExecutor()
-# thread = executor.submit(foo, args=('done!'))
-thread = executor.submit(blah)
-# thread.add_done_callback(cb)
-
-while thread.running():
+loop.run_until_complete(a_main())
+for x in range(100):
   print(done)
   time.sleep(1)
-
-print("Thread is no longer running, global is now {}".format(done))
-
