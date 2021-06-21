4101a38ef4c2f3f2193ed99722c593a130a5de38
Damon Becker <dbecker@webroot.com>
2021-06-21 17:55:48 -0600

    Multiple implementations of concurrency to try

--- python/async.py
+++ python/async.py
@@ -26,25 +26,73 @@
 
 # print("Thread is no longer running, global is now {}".format(done))
 
-import time
-import asyncio
+# import time
+# import asyncio
 
+# done = False
+
+# async def foo(bar):
+#   done = True
+#   return "foo with arg {}".format(bar)
+
+# async def blah():
+#   done = True
+#   return "blah"
+
+# async def a_main():
+#   task = asyncio.ensure_future(blah())
+#   print("In main")
+#   await task
+
+# loop.run_until_complete(a_main())
+# for x in range(100):
+#   print(done)
+#   time.sleep(1)
+
+from multiprocessing import Pool
+import time
+import os
 done = False
 
-async def foo(bar):
+def foo(bar):
+  time.sleep(5)
+  return "foo with arg {}".format(bar)
+
+def blah():
+  time.sleep(5)
+  return "blah"
+
+def cb():
   done = True
-  return "done with foo"
 
-async def blah():
-  done = True
-  return "done with blah"
-
-async def a_main():
-  task = asyncio.ensure_future(blah())
-  print("In main")
-  await task
-
-loop.run_until_complete(a_main())
-for x in range(100):
-  print(done)
+pool = Pool(processes=1)
+result = pool.apply_async(foo, ('bar',))
+timestamp = time.time()
+while not result.ready():
+  os.system('clear')
+  print("Not done yet, timer is {}".format(int(time.time() - timestamp) + 1))
   time.sleep(1)
+
+print("Done! Return value is {}".format(result.get()))
+
+# import threading
+# import time
+# import os
+
+# done = False
+
+# def foo(bar):
+#   time.sleep(5)
+#   bar = True
+
+# thread = threading.Thread(target=foo, args=(done,))
+# thread.start()
+
+# timestamp = time.time()
+
+# while thread.is_alive():
+#   os.system('clear')
+#   print("Not done yet, timer is {}".format(int(time.time() - timestamp) + 1))
+#   time.sleep(1)
+
+# print("Done! Semaphore is {}".format(done))
