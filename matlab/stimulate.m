10fb116dd81e5a8b3c531bc73649fb31b10f6cd7
GraysonButcher <54848272+GraysonButcher@users.noreply.github.com>
2021-09-22 14:23:22 -0500

    Reduced default stimulation duration from 5 seconds to 0.5 seconds

--- matlab/stimulate.m
+++ matlab/stimulate.m
@@ -1,7 +1,7 @@
 % Start a stimulation
 function stimulate
 
-    result = RodStimUI('stimulate','amplitude',1,'frequency',30,'pulse width',100,'duration',5,'callback',@stimEnd);
+    result = RodStimUI('stimulate','amplitude',1,'frequency',30,'pulse width',100,'duration',.5,'callback',@stimEnd);
     
     % Check if stimulation started successfully
     if strcmp(result,'success')
